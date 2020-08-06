79a7d863b86657ec0a8062175c246c4587b259b1
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2020-08-06 13:44:36 +0200

    more home

--- src/Web/Controller/DefaultController.php
+++ src/Web/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Web\Controller;
 class DefaultController extends Controller {
 
     public function run(){
+        $this->assign('menu', '404');
         $this->template('404.twig');
     }
 
