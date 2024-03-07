46ac91f3aca87752b948ade079abed654a5bd8b0
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2024-03-07 08:13:05 -0500

    links on footer

--- src/Web/Controller/Controller.php
+++ src/Web/Controller/Controller.php
@@ -23,7 +23,7 @@ abstract class Controller extends \Core\Controller\Controller
     {
         $this->defaultMetatags();
 
-        $this->assign('staticVersion', 34);
+        $this->assign('staticVersion', 35);
 
         $this->run();
     }
