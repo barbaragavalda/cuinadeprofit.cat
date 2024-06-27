70e3a79ea120583fb14ea0fec691a0bf388e305b
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2024-06-27 12:31:45 +0200

    new map style

--- src/Web/Controller/Controller.php
+++ src/Web/Controller/Controller.php
@@ -23,7 +23,7 @@ abstract class Controller extends \Core\Controller\Controller
     {
         $this->defaultMetatags();
 
-        $this->assign('staticVersion', 43);
+        $this->assign('staticVersion', 44);
 
         $this->run();
     }
