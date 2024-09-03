8c3fc56173550d1fea6dab9bf1ea7ab31d3ab9de
Bàrbara Gavaldà <barbaragavalda@gmail.com>
2024-09-03 11:10:03 +0200

    new font

--- src/Web/Controller/Controller.php
+++ src/Web/Controller/Controller.php
@@ -23,7 +23,7 @@ abstract class Controller extends \Core\Controller\Controller
     {
         $this->defaultMetatags();
 
-        $this->assign('staticVersion', 47);
+        $this->assign('staticVersion', 48);
 
         $this->run();
     }
